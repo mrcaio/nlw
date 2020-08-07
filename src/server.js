647e6e779abd57992a7c14487a305ac6091cfd78
Caio <caiobaader@gmail.com>
2020-08-07 20:29:44 -0300

    Aula 04
    continuação

--- src/server.js
+++ src/server.js
@@ -1,3 +1,4 @@
+//Dados
 const proffys = [
     {
     name: "Diego Fernandes", 
@@ -10,6 +11,7 @@ const proffys = [
     time_from: [720], 
     time_to: [1220],
 },
+
 {
     name: "Danilo Evangelista", 
     avatar: "https://avatars2.githubusercontent.com/u/2254731?s=460&amp;u=0ba16a79456c2f250e7579cb388fa18c5c2d7d65&amp;v=4",
@@ -22,21 +24,74 @@ const proffys = [
     time_to: [1220],
 }
 ]
+
+const subjects = [
+    "Artes",
+    "Biologia",
+    "Ciências",
+    "Educação física",
+    "Física",
+    "Geografia",
+    "História",
+    "Matemática",
+    "Português",
+    "Química",
+]
+
+const weekdays = [
+  "Domingo",
+  "Segunda-feira",
+  "Terça-feira",
+  "Quarta-feira",
+  "Quinta-feira",
+  "Sexta-feira",
+    "Sábado",
+]
+
+//Funcionalidades da aplicação
+function pageLanding (req, res) {
+    return res.render("index.html")
+}
+
+function pageStudy (req, res) {
+    const filters = req.query
+    return res.render("study.html", { proffys, filters, subjects, weekdays })
+}
+
+function pageGiveClasses (req, res) {
+    const data = req.query
+
+    //se tiver dados
+    const isNotEmpty = Object.keys(data).length > 0
+    if (isNotEmpty) {
+        //adicionar dados a lista de proffys
+    proffys.push(data)
+
+    return res.redirect("/study")
+
+    }
+
+    //se não, mostrar a página
+    return res.render("give-classes.html", {subjects, weekdays } )
+}
+
+// Servidor
 const express = require('express')
 const server = express()
+const nunjucks = require('nunjucks')
 
+//configurar nunjucks (template engie)
+nunjucks.configure('src/views', {
+    express: server,
+    noCache: true,
+})
+//Inicio e configuração do servidor
+server
+//configurar os arquivos estáticos (css, scripts, imagens)
 server.use(express.static("public"))
-
-.get("/", (req, res) => {
-    return res.sendFile(__dirname + "/views/index.html")
-})
-
-.get("/study", (req, res) => {
-    return res.sendFile(__dirname + "/views/study.html")
-})
-
-.get("/give-classes", (req, res) => {
-    return res.sendFile(__dirname + "/views/give-classes.html")
-})    
-
+//rotas da aplicação
+.get("/", pageLanding)
+.get("/study", pageStudy)
+.get("/give-classes", pageGiveClasses)
+//Start do Servidor
 .listen(5000)
